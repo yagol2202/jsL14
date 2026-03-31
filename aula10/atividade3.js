710fedc7f3eb0896cbf5db64306d33cf560c68af
yagol2202 <alvesy.s890@gmail.com>
2026-03-30 21:38:06 -0300

    Adiciona exercícios de JavaScript e estrutura HTML para Aula 10

---
+++ aula10/atividade3.js
@@ -0,0 +1,12 @@
+// Exercício 3
+let num1 = number(prompt("Digite o primeiro número:"));
+let num2 = number(prompt("Digite o segundo número:"));
+let = parseInt(num1 + num2);
+let resultadoSoma = num1 + num2 + 10;
+let resultadoSubtracao = num1 - num2;
+let resultadoMultiplicacao = num1 * num1;
+let resultadoDivisao = 600 / num2;
+console.log("Resultado da soma: " + resultadoSoma);
+console.log("Resultado da subtração: " + resultadoSubtracao);
+console.log("Resultado da multiplicação: " + resultadoMultiplicacao);
+console.log("Resultado da divisão: " + resultadoDivisao);
---
+++ aula10/script.js
@@ -0,0 +1,20 @@
+console.log("Olá, mundo!");
+console.log("Este é um exemplo de mensagem no console.");
+alert("Bem-vindo ao JavaScript!");
+//atividade 1
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Quantos anos você tem?");
+let cidade = prompt("De qual cidade você é?");
+let email = prompt("Qual é o seu email?");
+let profissao = prompt("Qual é a sua profissão?");
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Cidade: " + cidade);
+console.log("Email: " + email);
+console.log("Profissão: " + profissao);
+console.log("Oi eu sou " + nome + ", tenho " + idade + " anos, moro em " + cidade + ", meu email é " + email + " e minha profissão é " + profissao + ".");
+// Exercício 2
+let palavra = "JavaScript";
+let numero = 2026;
+let numeroDecimal = 3.14;
+let numeroString = "2026";
