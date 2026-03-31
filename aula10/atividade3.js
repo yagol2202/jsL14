cdc2837b82651b21847af69434ed148ae93623f1
yagol2202 <alvesy.s890@gmail.com>
2026-03-30 21:47:07 -0300

    Corrige a conversão de tipos para Number nas entradas do usuário

--- aula10/atividade3.js
+++ aula10/atividade3.js
@@ -1,11 +1,12 @@
 // Exercício 3
-let num1 = number(prompt("Digite o primeiro número:"));
-let num2 = number(prompt("Digite o segundo número:"));
+let num1 = Number(prompt("Digite o primeiro número:"));
+let num2 = Number(prompt("Digite o segundo número:"));
 let = parseInt(num1 + num2);
 let resultadoSoma = num1 + num2 + 10;
 let resultadoSubtracao = num1 - num2;
 let resultadoMultiplicacao = num1 * num1;
 let resultadoDivisao = 600 / num2;
+let = parseInt(resultadoDivisao);
 console.log("Resultado da soma: " + resultadoSoma);
 console.log("Resultado da subtração: " + resultadoSubtracao);
 console.log("Resultado da multiplicação: " + resultadoMultiplicacao);
