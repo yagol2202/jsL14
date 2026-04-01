5e2b6fcd0976ae0d918067cd805d12e7be3c204b
yagol2202 <alvesy.s890@gmail.com>
2026-04-01 19:57:13 -0300

    Atualiza o script para a atividade 4 e corrige a referência no HTML

---
+++ aula10/atividade4.js
@@ -0,0 +1,16 @@
+//atividade1
+let salario = Number(prompt("Digite o salário do funcionário:"));
+let aumento = salario * 0.20;
+let novoSalario = salario + aumento;
+console.log("O novo salário do funcionário é: " + novoSalario);
+//atividade 2
+let num1 = Number(prompt("Digite o primeiro número:"));
+let num2 = Number(prompt("Digite o segundo número:"));
+let num3 = Number(prompt("Digite o terceiro número:"));
+let media = (num1 + num2 + num3) / 3;
+console.log(`A média é: ${media.toFixed(2)}`);
+
+//atividade 3
+let lado = Number(prompt("Digite o lado do quadrado:"));
+let area = lado * lado;
+console.log("A área do quadrado é: " + area);
