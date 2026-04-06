8d146ba9af02ff0eba15c722db438e2fc1afbe32
yagol2202 <alvesy.s890@gmail.com>
2026-04-06 19:17:59 -0300

    Adiciona arquivos HTML e JavaScript para a atividade 12

---
+++ aula 12/switch.js
@@ -0,0 +1,17 @@
+switch (escolha) {
+    case Verde:
+        console.log("Voce tem 10% de desconto");
+        break;
+    case Amarelo:
+        console.log("Voce tem 20% de desconto");
+        break;    
+    case Azul:
+        console.log("Voce tem 30% de desconto");
+        break;
+    case Vermelho:
+        console.log("Voce tem 40% de desconto");
+        break;    
+    default:
+        console.log("Opcao invalida");
+        break;
+}
